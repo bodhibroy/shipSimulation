6bcb2a48abea6884c3dcef5a2fbcb77cb2fff07e
bodhi <bodhi@comp.nus.edu.sg>
2014-07-17 17:29:10 +0800

    added the first chunk of code. Multiple ships roaming around like anything

---
+++ gmapsBoating.js
@@ -0,0 +1,249 @@
+// Coordinate Things
+var coordThings = {
+	delta : function(dx, dy) {
+		return {dx: dx, dy: dy}
+	},
+	
+	sinDeg : function(angle) {
+		theta = Math.PI * angle / 180.0
+		return Math.sin(theta)
+	},
+	
+	cosDeg : function(angle) {
+		theta = Math.PI * angle / 180.0
+		return Math.cos(theta)
+	},
+	
+	makeVector : function(angle, M) {
+		theta = Math.PI * angle / 180.0
+		return {dx: M * Math.cos(theta), dy: M * Math.sin(theta)}
+	},	
+
+	rotate : function(pt, angle) {
+		theta = Math.PI * angle / 180.0
+		ct = Math.cos(theta)
+		st = Math.sin(theta)
+		pt2 = {dx: pt.dx * ct - pt.dy * st, dy: pt.dy * ct + pt.dx * st}
+		return pt2
+	},
+	
+	metresToDeg : function(pt) {
+		var degPerMetre = 1.0 / 111319.9
+		return {dlng: degPerMetre * pt.dx, dlat: degPerMetre * pt.dy}
+	},
+
+	degToMetres : function(ll) {
+		var metresPerDeg = 111319.9
+		return {dx: metresPerDeg * ll.dlat, dy: metresPerDeg * ll.dlng}
+	},
+
+	shiftMetric : function(v, dv) {
+		return {dx: v.dx + dv.dx, dy: v.dy + dv.dy}
+	},
+
+	shiftLatLng : function(ll, dll) {
+		return new google.maps.LatLng(ll.lat() + dll.dlat, ll.lng() + dll.dlng)
+	}
+}
+coordThings.shiftLatLngMetric = function(ll, dv) {
+	dll = coordThings.metresToDeg(dv)
+	return new google.maps.LatLng(ll.lat() + dll.dlat, ll.lng() + dll.dlng)
+}
+coordThings.pathFromMetricDeltas = function(refLL, dv, verts, angle) {
+	return verts.map(function(v) {
+		var shift = coordThings.shiftMetric(coordThings.rotate(v, angle), dv)
+		return coordThings.shiftLatLngMetric(refLL, shift)
+	})
+}
+
+
+
+// Make Ship
+var shipFactory = {}
+shipFactory.makeShipDims = function(length, breadth, fwd) {
+	return {length: length, breadth: breadth, fwd: fwd}
+}
+
+shipFactory.makeShipVerts = function(dims) {
+	return [	{dx:dims.length/2.0 + dims.fwd, dy:0},
+				{dx:dims.length/2.0, dy:dims.breadth/2.0},
+				{dx:-dims.length/2.0, dy:dims.breadth/2.0},
+				{dx:dims.fwd-dims.length/2.0, dy:0},
+				{dx:-dims.length/2.0, dy:-dims.breadth/2.0},
+				{dx:dims.length/2.0, dy:-dims.breadth/2.0}
+				]
+}
+
+shipFactory.makeShipDomainVerts = function(dims, safety_radius, fwd_distance, fwd_angle) {
+	dtc = 90.0 / 4.0
+	dto = fwd_angle / 4.0
+
+	return [	
+				{dx: 0, dy: dims.breadth/2.0 + safety_radius},
+
+				{dx: -dims.length/2.0 + safety_radius*coordThings.cosDeg(90), dy: dims.breadth/2.0 + safety_radius*coordThings.sinDeg(90)},
+				{dx: -dims.length/2.0 + safety_radius*coordThings.cosDeg(112.5), dy: dims.breadth/2.0 + safety_radius*coordThings.sinDeg(112.5)},
+				{dx: -dims.length/2.0 + safety_radius*coordThings.cosDeg(135), dy: dims.breadth/2.0 + safety_radius*coordThings.sinDeg(135)},
+				{dx: -dims.length/2.0 + safety_radius*coordThings.cosDeg(157.5), dy: dims.breadth/2.0 + safety_radius*coordThings.sinDeg(157.5)},
+				{dx: -dims.length/2.0 + safety_radius*coordThings.cosDeg(180), dy: dims.breadth/2.0 + safety_radius*coordThings.sinDeg(180)},
+
+				{dx: -dims.length/2.0 + safety_radius*coordThings.cosDeg(180), dy: -dims.breadth/2.0 + safety_radius*coordThings.sinDeg(180)},
+				{dx: -dims.length/2.0 + safety_radius*coordThings.cosDeg(202.5), dy: -dims.breadth/2.0 + safety_radius*coordThings.sinDeg(202.5)},
+				{dx: -dims.length/2.0 + safety_radius*coordThings.cosDeg(225), dy: -dims.breadth/2.0 + safety_radius*coordThings.sinDeg(225)},
+				{dx: -dims.length/2.0 + safety_radius*coordThings.cosDeg(247.5), dy: -dims.breadth/2.0 + safety_radius*coordThings.sinDeg(247.5)},
+				{dx: -dims.length/2.0 + safety_radius*coordThings.cosDeg(270), dy: -dims.breadth/2.0 + safety_radius*coordThings.sinDeg(270)},
+
+				{dx: 0, dy: -dims.breadth/2.0 - safety_radius},
+
+				{	dx: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+0*dtc))*(coordThings.cosDeg(-fwd_angle+0*dto)) + (-dims.breadth/2+safety_radius*coordThings.sinDeg(-90+0*dtc))*(-coordThings.sinDeg(-fwd_angle+0*dto)),
+					dy: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+0*dtc))*(coordThings.sinDeg(-fwd_angle+0*dto)) + (-dims.breadth/2+safety_radius*coordThings.sinDeg(-90+0*dtc))*(coordThings.cosDeg(-fwd_angle+0*dto))
+				},
+				{	dx: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+1*dtc))*(coordThings.cosDeg(-fwd_angle+1*dto)) + (-dims.breadth/2+safety_radius*coordThings.sinDeg(-90+1*dtc))*(-coordThings.sinDeg(-fwd_angle+1*dto)),
+					dy: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+1*dtc))*(coordThings.sinDeg(-fwd_angle+1*dto)) + (-dims.breadth/2+safety_radius*coordThings.sinDeg(-90+1*dtc))*(coordThings.cosDeg(-fwd_angle+1*dto))
+				},
+				{	dx: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+2*dtc))*(coordThings.cosDeg(-fwd_angle+2*dto)) + (-dims.breadth/2+safety_radius*coordThings.sinDeg(-90+2*dtc))*(-coordThings.sinDeg(-fwd_angle+2*dto)),
+					dy: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+2*dtc))*(coordThings.sinDeg(-fwd_angle+2*dto)) + (-dims.breadth/2+safety_radius*coordThings.sinDeg(-90+2*dtc))*(coordThings.cosDeg(-fwd_angle+2*dto))
+				},
+				{	dx: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+3*dtc))*(coordThings.cosDeg(-fwd_angle+3*dto)) + (-dims.breadth/2+safety_radius*coordThings.sinDeg(-90+3*dtc))*(-coordThings.sinDeg(-fwd_angle+3*dto)),
+					dy: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+3*dtc))*(coordThings.sinDeg(-fwd_angle+3*dto)) + (-dims.breadth/2+safety_radius*coordThings.sinDeg(-90+3*dtc))*(coordThings.cosDeg(-fwd_angle+3*dto))
+				},
+				{	dx: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+4*dtc))*(coordThings.cosDeg(-fwd_angle+4*dto)) + (-dims.breadth/2+safety_radius*coordThings.sinDeg(-90+4*dtc))*(-coordThings.sinDeg(-fwd_angle+4*dto)),
+					dy: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+4*dtc))*(coordThings.sinDeg(-fwd_angle+4*dto)) + (-dims.breadth/2+safety_radius*coordThings.sinDeg(-90+4*dtc))*(coordThings.cosDeg(-fwd_angle+4*dto))
+				},
+
+				{	dx: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+4*dtc))*(coordThings.cosDeg(-fwd_angle+4*dto)) + (dims.breadth/2+safety_radius*coordThings.sinDeg(-90+4*dtc))*(-coordThings.sinDeg(-fwd_angle+4*dto)),
+					dy: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+4*dtc))*(coordThings.sinDeg(-fwd_angle+4*dto)) + (dims.breadth/2+safety_radius*coordThings.sinDeg(-90+4*dtc))*(coordThings.cosDeg(-fwd_angle+4*dto))
+				},
+				{	dx: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+5*dtc))*(coordThings.cosDeg(-fwd_angle+5*dto)) + (dims.breadth/2+safety_radius*coordThings.sinDeg(-90+5*dtc))*(-coordThings.sinDeg(-fwd_angle+5*dto)),
+					dy: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+5*dtc))*(coordThings.sinDeg(-fwd_angle+5*dto)) + (dims.breadth/2+safety_radius*coordThings.sinDeg(-90+5*dtc))*(coordThings.cosDeg(-fwd_angle+5*dto))
+				},
+				{	dx: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+6*dtc))*(coordThings.cosDeg(-fwd_angle+6*dto)) + (dims.breadth/2+safety_radius*coordThings.sinDeg(-90+6*dtc))*(-coordThings.sinDeg(-fwd_angle+6*dto)),
+					dy: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+6*dtc))*(coordThings.sinDeg(-fwd_angle+6*dto)) + (dims.breadth/2+safety_radius*coordThings.sinDeg(-90+6*dtc))*(coordThings.cosDeg(-fwd_angle+6*dto))
+				},
+				{	dx: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+7*dtc))*(coordThings.cosDeg(-fwd_angle+7*dto)) + (dims.breadth/2+safety_radius*coordThings.sinDeg(-90+7*dtc))*(-coordThings.sinDeg(-fwd_angle+7*dto)),
+					dy: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+7*dtc))*(coordThings.sinDeg(-fwd_angle+7*dto)) + (dims.breadth/2+safety_radius*coordThings.sinDeg(-90+7*dtc))*(coordThings.cosDeg(-fwd_angle+7*dto))
+				},
+				{	dx: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+8*dtc))*(coordThings.cosDeg(-fwd_angle+8*dto)) + (dims.breadth/2+safety_radius*coordThings.sinDeg(-90+8*dtc))*(-coordThings.sinDeg(-fwd_angle+8*dto)),
+					dy: (dims.length/2+fwd_distance+safety_radius*coordThings.cosDeg(-90+8*dtc))*(coordThings.sinDeg(-fwd_angle+8*dto)) + (dims.breadth/2+safety_radius*coordThings.sinDeg(-90+8*dtc))*(coordThings.cosDeg(-fwd_angle+8*dto))
+				},
+
+				]
+}
+
+shipFactory.makeShipPath = function(dims, angle, basePosition, dv) {
+	var verts = shipFactory.makeShipVerts(dims)
+	var path = coordThings.pathFromMetricDeltas(basePosition, dv, verts, angle)
+	return path
+}
+	
+shipFactory.makeShip = function(dims, basePosition, v, theta) {
+	var ship = {}
+
+	ship.__dims = dims
+	ship.basePosition = basePosition
+	ship.v = v
+	ship.theta = theta
+	ship.shipVerts = shipFactory.makeShipVerts(dims)
+	ship.safetyRadius = 0
+	ship.fwdDistance = 0
+	ship.fwdAngle = 0
+
+	ship.updatePosition = function(_v, _theta) {
+		ship.v = _v
+		ship.theta = _theta
+	}
+	ship.updateDomainParams = function(_safety_radius, _fwd_distance, _fwd_angle) {
+		ship.safetyRadius = _safety_radius
+		ship.fwdDistance = _fwd_distance
+		ship.fwdAngle = _fwd_angle
+	}
+	ship.makeShipPath = function() {
+		var path = coordThings.pathFromMetricDeltas(ship.basePosition, ship.v, ship.shipVerts, ship.theta)
+		return path
+	}
+	ship.makeDomainPath = function() {
+		var domainVerts = shipFactory.makeShipDomainVerts(dims, ship.safetyRadius, ship.fwdDistance, ship.fwdAngle)
+		var path = coordThings.pathFromMetricDeltas(ship.basePosition, ship.v, domainVerts, ship.theta)
+		return path
+	}
+	ship.makeShipPoly = function(strokeColor, fillColor) {
+		var path = ship.makeShipPath()
+		poly = new google.maps.Polygon({
+			path: path,
+			strokeColor: strokeColor,
+			strokeOpacity: 0.8,
+			fillColor: fillColor,
+			fillOpacity: 0.4,
+			strokeWeight: 2
+		})
+		ship._shipPoly = poly
+		return poly
+	}
+	ship.setShipPolyOptions = function(strokeColor, fillColor, strokeOpacity, fillOpacity) {
+		if (ship._shipPoly) {
+			ship._shipPoly.setOptions({
+				strokeColor: strokeColor, 
+				fillColor: fillColor, 
+				strokeOpacity: strokeOpacity, 
+				fillOpacity: fillOpacity
+			});
+		}
+	}
+	ship.setShipDomainPolyOptions = function(strokeColor, fillColor, strokeOpacity, fillOpacity) {
+		if (ship._shipDomainPoly) {
+			ship._shipDomainPoly.setOptions({
+				strokeColor: strokeColor, 
+				fillColor: fillColor, 
+				strokeOpacity: strokeOpacity, 
+				fillOpacity: fillOpacity
+			});
+		}
+	}
+	ship.updateShipPoly = function() {
+		if (ship._shipPoly) {
+			var path = ship.makeShipPath()
+			vertices = ship._shipPoly.getPath()
+			for (var i = 0; i < vertices.getLength(); i++) {
+		    	vertices.setAt(i, path[i])
+			}
+		}
+	}
+	ship.makeDomainPoly = function(strokeColor, fillColor) {
+		var path = ship.makeDomainPath()
+		poly = new google.maps.Polygon({
+			path: path,
+			strokeColor: strokeColor,
+			strokeOpacity: 0.1,
+			fillColor: fillColor,
+			fillOpacity: 0.1,
+			strokeWeight: 2
+		})
+		ship._shipDomainPoly = poly
+		return poly
+	}
+	ship.updateDomainPoly = function() {
+		if (ship._shipDomainPoly) {
+			var path = ship.makeDomainPath()
+			vertices = ship._shipDomainPoly.getPath()
+			for (var i = 0; i < vertices.getLength(); i++) {
+			    vertices.setAt(i, path[i])
+			}
+		}
+	}
+	ship.updatePolys = function() {
+		ship.updateShipPoly()
+		ship.updateDomainPoly()
+	}
+	ship.putPolysOnMap = function(map) {
+		ship._shipDomainPoly.setMap(map);
+		ship._shipPoly.setMap(map);
+	}
+	_shipPoly = ship.makeShipPoly('#00ff00', '#00ff00')
+	_shipDomainPoly = ship.makeDomainPoly('#ff0000', '#ff0000')
+
+	return ship
+}
+
+shipFactory.makeEZShip = function(dims, basePosition) {
+	return shipFactory.makeShip(dims, basePosition, coordThings.delta(0,0), 0) 
+}
